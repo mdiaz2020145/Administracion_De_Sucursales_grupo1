36638f7f1dae0323b65ca1378efeb3c46fe18d01
Marcelo Diaz <88795578+mdiaz2020145@users.noreply.github.com>
2022-03-29 22:31:31 -0600

    Update empresas.routes.js

--- src/routes/empresas.routes.js
+++ src/routes/empresas.routes.js
@@ -2,10 +2,9 @@ const express = require('express');
 const empresaControlador = require('../controllers/empresas.controller');
 
 const api = express.Router();
-
 api.post('/agregarE', empresaControlador.AgregarEmpresa);
 api.put('/editarE/:idEmpresa', empresaControlador.EditarEmpresa);
 api.delete('/eliminarE/:idEmpresa', empresaControlador.EliminarEmpresa);
-api.get('/encontrarE', empresaControlador.BuscarEmpresa);
+api.get('/encontrarE', empresaControlador.BuscarEmpresa);Lo
 
 module.exports = api;
