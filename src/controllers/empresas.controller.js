d879439e8554edbbb130205063153cabab20a8b5
cblanco2017162 <cblanco-2017162@kinal.edu.gt>
2022-03-29 19:16:14 -0600

    Crud de empresas

--- src/controllers/empresas.controller.js
+++ src/controllers/empresas.controller.js
@@ -1,84 +1,70 @@
-const Empresa=require('../models/empresas.model')
-const underscore=require('underscore');
-const bcrypt=require('bcrypt-nodejs');
-const jwt=require('jwt-simple')
+const Empresa = require('../models/empresas.model');
+const bcrypt = require('bcrypt-nodejs');
 
-function registrar(req, res){
-    var parametros=req.body;
-    var empresaModel=new Empresa;
-    Usuario.findOne({usuario: parametros.usuario}, (err, empresaEncontrada)=>{
-        if(underscore.isEmpty(empresaEncontrada)){
-            empresaModel.usuario=parametros.usuario;
-            empresaModel.nombreEmpresa=parametros.nombre;
-            empresaModel.rol='EMPRESA';
-            empresaModel.tipoEmpresa=parametros.tipoEmpresa;
-            bcrypt.hash(parametros.password,null,null, (err,passwordEncriptada)=>{
-                empresaModel.password=passwordEncriptada
-                empresaModel.save(()=>{});
-            })
-        }else{
-            return res.status(500).send({mensaje: "El nombre de usuario ya esta en uso, utilice uno diferente"})
-        }
-    })
-}
-
-
-
-function creacionAdmin() {
-    var empresaModel = new Empresa();
-    Empresa.findOne({ usuario: 'SuperAdmin' }, (err, usuarioEncontrado) => {
-
-        if (underscore.isEmpty(usuarioEncontrado)) {
-
-            empresaModel.usuario = 'SuperAdmin';
-            empresaModel.nombre = 'Administrador por Defecto';
-            empresaModel.rol = 'ADMIN';
-
-            bcrypt.hash('123456', null, null, (err, passwordEncriptada) => {
-                empresaModel.password = passwordEncriptada;
-
-                empresaModel.save(() => {
-                    console.log("Administrador creado con exito")
-                });
-
-            });
-        } else {
-            console.log("El administrador ya existe")
-        }
-    })
-}
-
-function login(req, res) {
+function AgregarEmpresa (req, res){
     var parametros = req.body;
-    Empresa.findOne({ usuario: parametros.usuario }, (err, empresaEncontrada) => {
-        if (err) return res.status(500).send({ mensaje: 'Error en la peticion' });
-        if (underscore.isEmpty(empresaEncontrada)) {
-            bcrypt.compare(parametros.password, empresaEncontrada.password,
-                (err, verificacionPassword) => {
-                    if (verificacionPassword) {
-                        if (parametros.obtenerToken === 'true') {
-                            return res.status(200)
-                                .send({ token: jwt.crearToken(empresaEncontrada) })
-                        } else {
-                            empresaEncontrada.password = undefined;
-                            return res.status(200)
-                                .send({ empresa: "Parametro faltante" })
-                        }
+    var empresaModelo = new Empresa();
 
-                    } else {
-                        return res.status(500)
-                            .send({ mensaje: 'La clave no coincide' });
-                    }
-                })
-        } else {
-            return res.status(500)
-                .send({ mensaje: 'Error, la empresa no se encuentra registrada.' })
-        }
+    if( parametros.usuario && parametros.nombreEmpresa && parametros.password && parametros.tipoEmpresa) {
+        empresaModelo.email = parametros.email;
+        empresaModelo.password = parametros.password;
+        empresaModelo.rol = 'EMPRESA';
+        empresaModelo.tipoEmpresa = parametros.tipoEmpresa;
+
+        bcrypt.hash(parametros.password, null, null, (err, passwordEncriptada) => {
+            empresaModelo.password = passwordEncriptada;
+
+                 empresaModelo.save((err, empresaGuardada) => {
+                       if(err) return res.status(500).send({ mensaje: "Error en la peticion" });
+                       if(!empresaGuardada) return res.status(404).send( { mensaje: "Error, no se agrego ninguna empresa"});
+
+                 return res.status(200).send({ empresa: empresaGuardada });
+        })
+      })
+    }
+}
+
+
+function EditarEmpresa (req, res) {
+    var idE = req.params.idEmpresa;
+    var parametros = req.body;
+    
+        Empresa.findByIdAndUpdate(idE, parametros, { new: true } ,(err, empresaActualizada) => {
+        if (err) return res.status(500).send({ mensaje: 'Error en la peticion'});
+        if(!empresaActualizada) return res.status(404).send( { mensaje: 'Error al editar la empresa'});
+
+        return res.status(200).send({ empresa: empresaActualizada});
+    });
+}
+
+function EliminarEmpresa(req, res){
+    var id = req.params.idEmpresa;
+    
+    Empresa.findByIdAndDelete(id, (err, empresaEliminada) => {
+        if(err) return res.status(500).send({ mensaje: 'Error en la peticion'});
+        if(!empresaEliminada) return res.status(404).send( { mensaje: 'Error al eliminar la empresa'});
+
+        return res.status(200).send({ empresa: empresaEliminada});
     })
 }
 
-module.exports ={
-    creacionAdmin,
-    registrar,
-    login
+function BuscarEmpresa(req, res){
+    Empresa.find((err, empresaEncontrada) => {
+        if (err) return res.send({ mensaje: "Error: " + err })
+
+        for (let i = 0; i < empresaEncontrada.length; i++) {
+            console.log(empresaEncontrada[i].nombre)
+        }
+
+        return res.send({ empresa: empresaEncontrada });
+    })
+}
+
+
+
+module.exports = {
+    AgregarEmpresa,
+    EditarEmpresa,
+    EliminarEmpresa,
+    BuscarEmpresa
 }
--- src/routes/empresas.routes.js
+++ src/routes/empresas.routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const empresaController = require('../controllers/empresas.controller');
-//const md_autenticacion = require('../middlewares/autenticacion');
+const empresaControlador = require('../controllers/empresas.controller');
+
 const api = express.Router();
 
-api.post('/registrar', empresaController.registrar);
-api.post('/login', empresaController.login);
+api.post('/agregarE', empresaControlador.AgregarEmpresa);
+api.put('/editarE/:idEmpresa', empresaControlador.EditarEmpresa);
+api.delete('/eliminarE/:idEmpresa', empresaControlador.EliminarEmpresa);
+api.get('/encontrarE', empresaControlador.BuscarEmpresa);
+
 module.exports = api;
